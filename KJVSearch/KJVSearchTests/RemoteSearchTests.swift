f57f304a25100446e6f05698bb562fef7ac2188c
Paulo Silva <paulobfsilva@gmail.com>
2022-10-26 10:42:56 +0100

    Move test logic to a separate type HTTPClientSpy

--- KJVSearch/KJVSearchTests/RemoteSearchTests.swift
+++ KJVSearch/KJVSearchTests/RemoteSearchTests.swift
@@ -9,27 +9,36 @@ import XCTest
 
 class RemoteSearchLoader {
     func load() {
-        HTTPClient.shared.requestedURL = URL(string: "https://a.url.com")
+        HTTPClient.shared.get(from: URL(string: "https://a.url.com")!)
     }
 }
 
 class HTTPClient {
-    static let shared = HTTPClient()
-    private init() {}
+    static var shared = HTTPClient()
+    
+    func get(from url: URL) {}
+}
+
+class HTTPClientSpy: HTTPClient {
+    override func get(from url: URL) {
+        requestedURL = url
+    }
     
     var requestedURL: URL?
 }
 
 class RemoteSearchTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         _ = RemoteSearchLoader()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let sut = RemoteSearchLoader()
         
         sut.load()
