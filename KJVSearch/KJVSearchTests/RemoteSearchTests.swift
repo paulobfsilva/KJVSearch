601f1d4248e5b49cfa57b211338d4d5138b2ffee
Paulo Silva <paulobfsilva@gmail.com>
2022-10-27 11:00:49 +0100

    Improve readability with helper method

--- KJVSearch/KJVSearchTests/RemoteSearchTests.swift
+++ KJVSearch/KJVSearchTests/RemoteSearchTests.swift
@@ -40,7 +40,7 @@ class RemoteSearchTests: XCTestCase {
         var capturedErrors = [RemoteSearchLoader.Error]()
         sut.load { capturedErrors.append($0) }
         let clientError = NSError(domain: "Test", code: 0)
-        client.completions[0](clientError)
+        client.complete(with: clientError)
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
     
@@ -53,6 +53,10 @@ class RemoteSearchTests: XCTestCase {
         func get(from url: URL, completion: @escaping (Error) -> Void) {
             completions.append(completion)
             requestedURLs.append(url)
+        }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            completions[index](error)
         }
     }
     
