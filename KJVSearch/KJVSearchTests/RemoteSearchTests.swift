9f882fe16b5f7a5b53ef7c8a36ac7b9b72891db7
Paulo Silva <paulobfsilva@gmail.com>
2022-10-27 10:58:08 +0100

    Replace stubbing by capturing values instead

--- KJVSearch/KJVSearchTests/RemoteSearchTests.swift
+++ KJVSearch/KJVSearchTests/RemoteSearchTests.swift
@@ -36,10 +36,11 @@ class RemoteSearchTests: XCTestCase {
     
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-        client.error = NSError(domain: "Test", code: 0)
+        
         var capturedErrors = [RemoteSearchLoader.Error]()
         sut.load { capturedErrors.append($0) }
-        
+        let clientError = NSError(domain: "Test", code: 0)
+        client.completions[0](clientError)
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
     
@@ -47,12 +48,10 @@ class RemoteSearchTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
-        var error: Error?
+        var completions = [(Error) -> Void]()
         
         func get(from url: URL, completion: @escaping (Error) -> Void) {
-            if let error = error {
-                completion(error)
-            }
+            completions.append(completion)
             requestedURLs.append(url)
         }
     }
