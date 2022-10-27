4a9c04ca187c504f4700c02eafd5aa5f4c5a8474
Paulo Silva <paulobfsilva@gmail.com>
2022-10-27 16:59:11 +0100

    Remove duplicated test code by reusing 'expect' helper method

--- KJVSearch/KJVSearchTests/RemoteSearchTests.swift
+++ KJVSearch/KJVSearchTests/RemoteSearchTests.swift
@@ -36,12 +36,10 @@ class RemoteSearchTests: XCTestCase {
     
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-        
-        var capturedErrors = [RemoteSearchLoader.Error]()
-        sut.load { capturedErrors.append($0) }
-        let clientError = NSError(domain: "Test", code: 0)
-        client.complete(with: clientError)
-        XCTAssertEqual(capturedErrors, [.connectivity])
+        expect(sut, toCompleteWithError: .connectivity, when: {
+            let clientError = NSError(domain: "Test", code: 0)
+            client.complete(with: clientError)
+        })
     }
     
     func test_load_deliversErrorOnNon200HTTPResponse() {
@@ -49,27 +47,38 @@ class RemoteSearchTests: XCTestCase {
         
         let samples = [199, 201, 300, 400, 401, 402, 403, 404, 405, 406, 500]
         samples.enumerated().forEach { index, code in
-            var capturedErrors = [RemoteSearchLoader.Error]()
-            sut.load { capturedErrors.append($0) }
-            
-            client.complete(withStatusCode: code, at: index)
-            XCTAssertEqual(capturedErrors, [.invalidData])
-            capturedErrors = []
+            expect(sut, toCompleteWithError: .invalidData, when: {
+                client.complete(withStatusCode: code, at: index)
+            })
         }
         
     }
     
     func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
         let (sut, client) = makeSUT()
-        var capturedErrors = [RemoteSearchLoader.Error]()
-        sut.load { capturedErrors.append($0) }
-        
-        let invalidJSON = Data("invalid JSON".utf8)
-        client.complete(withStatusCode: 200, data: invalidJSON)
-        XCTAssertEqual(capturedErrors, [.invalidData])
+        expect(sut, toCompleteWithError: .invalidData, when: {
+            let invalidJSON = Data("invalid JSON".utf8)
+            client.complete(withStatusCode: 200, data: invalidJSON)
+        })
     }
     
     // MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!) -> (sut: RemoteSearchLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteSearchLoader(url: url, client: client)
+        
+        return (sut, client)
+    }
+    
+    private func expect(_ sut: RemoteSearchLoader, toCompleteWithError error: RemoteSearchLoader.Error, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+        var capturedErrors = [RemoteSearchLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+        
+        action()
+        
+        XCTAssertEqual(capturedErrors, [error], file: file, line: line)
+    }
     
     private class HTTPClientSpy: HTTPClient {
         
@@ -95,12 +104,5 @@ class RemoteSearchTests: XCTestCase {
             )!
             messages[index].completion(.success(data, response))
         }
-    }
-    
-    private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!) -> (sut: RemoteSearchLoader, client: HTTPClientSpy) {
-        let client = HTTPClientSpy()
-        let sut = RemoteSearchLoader(url: url, client: client)
-        
-        return (sut, client)
     }
 }
