fc9277603744e1f5176bb79dd233f83c4a8c3ba7
Paulo Silva <paulobfsilva@gmail.com>
2022-10-28 14:00:58 +0100

    Move HTTPClient to its own file

---
+++ KJVSearch/KJVSearch/Search API/HTTPClient.swift
@@ -0,0 +1,17 @@
+//
+//  HTTPClient.swift
+//  KJVSearch
+//
+//  Created by Paulo Silva on 28/10/2022.
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
