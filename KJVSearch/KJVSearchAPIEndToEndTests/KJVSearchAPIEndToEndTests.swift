30ad8f62d64431fc0346d8cb8af7520ecb46b6cf
Paulo Silva <paulobfsilva@gmail.com>
2022-11-01 14:46:35 +0000

    Add memory leak tracking for components in integration

--- KJVSearch/KJVSearchAPIEndToEndTests/KJVSearchAPIEndToEndTests.swift
+++ KJVSearch/KJVSearchAPIEndToEndTests/KJVSearchAPIEndToEndTests.swift
@@ -27,11 +27,13 @@ class KJVSearchAPIEndToEndTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private func getSearchResult() -> LoadSearchResult? {
+    private func getSearchResult(file: StaticString = #filePath, line: UInt = #line) -> LoadSearchResult? {
         let tokenManager = AuthenticationTokenManager()
         let serverURL = URL(string: "https://www.nyckel.com/v0.9/functions/ieydm3vaouviuob1/search?sampleCount=10&includeData=true")!
         let client = URLSessionHTTPClient(tokenManager: tokenManager)
         let loader = RemoteSearchLoader(url: serverURL, client: client, query: "What is the Holy Ghost")
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(loader, file: file, line: line)
         
         let exp = expectation(description: "Wait for completion")
         
