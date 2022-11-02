59c98b52d707006ffb0f17420cae7716467bad22
Paulo Silva <paulobfsilva@gmail.com>
2022-11-02 15:54:37 +0000

    Save command requests cache deletion

--- KJVSearch/KJVSearchTests/Search Cache/CacheSearchUseCaseTests.swift
+++ KJVSearch/KJVSearchTests/Search Cache/CacheSearchUseCaseTests.swift
@@ -5,14 +5,27 @@
 //  Created by Paulo Silva on 02/11/2022.
 //
 
+import KJVSearch
 import XCTest
 
 class LocalSearchLoader {
-    init(store: SearchStore) {}
+    private let store: SearchStore
+    
+    init(store: SearchStore) {
+        self.store = store
+    }
+    
+    func save(_ items: [SearchItem]) {
+        store.deleteCachedSearch()
+    }
 }
 
 class SearchStore {
     var deleteCachedSearchCallCount = 0
+    
+    func deleteCachedSearch() {
+        deleteCachedSearchCallCount += 1
+    }
 }
 
 class CacheSearchUseCaseTests: XCTestCase {
@@ -22,5 +35,20 @@ class CacheSearchUseCaseTests: XCTestCase {
         _ = LocalSearchLoader(store: store)
         XCTAssertEqual(store.deleteCachedSearchCallCount, 0)
     }
-
+    
+    func test_save_requestsCacheDeletion() {
+        let store = SearchStore()
+        let sut = LocalSearchLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        
+        sut.save(items)
+        XCTAssertEqual(store.deleteCachedSearchCallCount, 1)
+    }
+    
+    // MARK: - Helpers
+    private func uniqueItem() -> SearchItem {
+        // sampleId is what makes a SearchItem unique
+        return SearchItem(sampleId: UUID().uuidString, distance: 0.5, externalId: "externalId", data: "data")
+    }
+    
 }
