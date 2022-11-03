e29061980703959664aef177b03f1ae4b97ad2d3
Paulo Silva <paulobfsilva@gmail.com>
2022-11-03 08:33:01 +0000

    Move 'LocalSearchItem' to its own file

--- KJVSearch/KJVSearch/Search Cache/SearchStore.swift
+++ KJVSearch/KJVSearch/Search Cache/SearchStore.swift
@@ -14,17 +14,3 @@ public protocol SearchStore {
     func deleteCachedSearch(completion: @escaping DeletionCompletion)
     func insert(_ items: [LocalSearchItem], timestamp: Date, completion: @escaping InsertionCompletion)
 }
-
-public struct LocalSearchItem: Equatable, Decodable {
-    public let sampleId: String
-    public let distance: Double
-    public let externalId: String
-    public let data: String
-    
-    public init(sampleId: String, distance: Double, externalId: String, data: String) {
-        self.sampleId = sampleId
-        self.distance = distance
-        self.externalId = externalId
-        self.data = data
-    }
-}
