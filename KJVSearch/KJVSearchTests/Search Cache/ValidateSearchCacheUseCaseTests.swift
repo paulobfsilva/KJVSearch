aa58b4da2e36eefc11affd393502197eb0a8284c
Paulo Silva <paulobfsilva@gmail.com>
2022-11-03 16:41:17 +0000

    LocalSearchLoader does not message store upon creation (before validating the cached feed)

---
+++ KJVSearch/KJVSearchTests/Search Cache/ValidateSearchCacheUseCaseTests.swift
@@ -0,0 +1,26 @@
+//
+//  ValidateSearchCacheUseCaseTests.swift
+//  KJVSearchTests
+//
+//  Created by Paulo Silva on 03/11/2022.
+//
+
+import KJVSearch
+import XCTest
+
+class ValidateSearchCacheUseCaseTests: XCTestCase {
+
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+
+    // MARK: - Helpers
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalSearchLoader, store: SearchStoreSpy) {
+        let store = SearchStoreSpy()
+        let sut = LocalSearchLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+}
