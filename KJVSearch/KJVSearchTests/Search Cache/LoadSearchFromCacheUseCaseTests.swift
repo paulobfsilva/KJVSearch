c19bdbf45ed140c97a75d65e16d2399ffb7132d5
Paulo Silva <paulobfsilva@gmail.com>
2022-11-03 09:49:40 +0000

    LocalSearchLoader does not message store upon creation (before loading the feed from the cache store)

---
+++ KJVSearch/KJVSearchTests/Search Cache/LoadSearchFromCacheUseCaseTests.swift
@@ -0,0 +1,65 @@
+//
+//  LoadSearchFromCacheUseCaseTests.swift
+//  KJVSearchTests
+//
+//  Created by Paulo Silva on 03/11/2022.
+//
+
+import KJVSearch
+import XCTest
+
+class LoadSearchFromCacheUseCaseTests: XCTestCase {
+
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+
+    // MARK: - Helpers
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalSearchLoader, store: SearchStoreSpy) {
+        let store = SearchStoreSpy()
+        let sut = LocalSearchLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+    
+    private class SearchStoreSpy: SearchStore {
+        
+        enum ReceivedMessage: Equatable {
+            case deleteCachedSearch
+            case insert([LocalSearchItem], Date)
+        }
+        
+        private(set) var receivedMessages = [ReceivedMessage]()
+        
+        private var deletionCompletions = [DeletionCompletion]()
+        private var insertionCompletions = [InsertionCompletion]()
+        
+        func deleteCachedSearch(completion: @escaping DeletionCompletion) {
+            deletionCompletions.append(completion)
+            receivedMessages.append(.deleteCachedSearch)
+        }
+        
+        func completeDeletion(with error: Error, at index: Int = 0) {
+            deletionCompletions[index](error)
+        }
+        
+        func completeDeletionSuccessfully(at index: Int = 0) {
+            deletionCompletions[index](nil)
+        }
+        
+        func insert(_ items: [LocalSearchItem], timestamp: Date, completion: @escaping InsertionCompletion) {
+            insertionCompletions.append(completion)
+            receivedMessages.append(.insert(items, timestamp))
+        }
+        
+        func completeInsertion(with error: Error, at index: Int = 0) {
+            insertionCompletions[index](error)
+        }
+        
+        func completeInsertionSuccessfully(at index: Int = 0) {
+            insertionCompletions[index](nil)
+        }
+    }
+}
