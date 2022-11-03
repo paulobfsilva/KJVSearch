7985ea82c9ab11552a8b2d7abb95f12a01f9b40c
Paulo Silva <paulobfsilva@gmail.com>
2022-11-03 21:17:36 +0000

    Move 'SearchCachePolicy' to its own file

--- KJVSearch/KJVSearch/Search Cache/LocalSearchLoader.swift
+++ KJVSearch/KJVSearch/Search Cache/LocalSearchLoader.swift
@@ -7,23 +7,6 @@
 
 import Foundation
 
-private final class SearchCachePolicy {
-    private init() {}
-    
-    private static let calendar = Calendar(identifier: .gregorian)
-    
-    private static var maxCacheAgeInDays: Int {
-        return 30
-    }
-    
-    static func validate(_ timestamp: Date, against date: Date) -> Bool {
-        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
-            return false
-        }
-        return date < maxCacheAge
-    }
-}
-
 public final class LocalSearchLoader {
     private let store: SearchStore
     private let currentDate: () -> Date
