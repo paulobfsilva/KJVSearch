87a3afa21280d9076425c06b0d50fadf3ae801e7
Paulo Silva <paulobfsilva@gmail.com>
2022-11-05 22:38:01 +0000

    Retrieve delivers failure on retrieval error (invalid cached data)

--- KJVSearch/KJVSearchTests/Search Cache/CodableSearchStoreTests.swift
+++ KJVSearch/KJVSearchTests/Search Cache/CodableSearchStoreTests.swift
@@ -48,9 +48,13 @@ class CodableSearchStore {
             return completion(.empty)
         }
         
-        let decoder = JSONDecoder()
-        let cache = try! decoder.decode(Cache.self, from: data)
-        completion(.found(results: cache.localSearchResults, timestamp: cache.timestamp))
+        do {
+            let decoder = JSONDecoder()
+            let cache = try decoder.decode(Cache.self, from: data)
+            completion(.found(results: cache.localSearchResults, timestamp: cache.timestamp))
+        } catch {
+            completion(.failure(error))
+        }
     }
     
     func insert(_ items: [LocalSearchItem], timestamp: Date, completion: @escaping SearchStore.InsertionCompletion) {
@@ -106,6 +110,14 @@ class CodableSearchStoreTests: XCTestCase {
         expect(sut, toRetrieveTwice: .found(results: searchResults, timestamp: timestamp))
     }
     
+    func test_retrieve_deliversFailureOnRetrievalError() {
+        let sut = makeSUT()
+        
+        try! "invalid data".write(to: testSpecificStoreURL(), atomically: false, encoding: .utf8)
+        
+        expect(sut, toRetrieve: .failure(anyError()))
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CodableSearchStore {
@@ -133,7 +145,7 @@ class CodableSearchStoreTests: XCTestCase {
         
         sut.retrieve { retrievedResult in
             switch (expectedResult, retrievedResult) {
-            case (.empty, .empty):
+            case (.empty, .empty), (.failure, .failure):
                 break
             case let (.found(expectedResult, expectedTimestamp), .found(retrievedResult, retrievedTimestamp)):
                 XCTAssertEqual(retrievedResult, expectedResult, file: file, line: line)
