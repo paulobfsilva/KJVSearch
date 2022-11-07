c9b80d620e56955d0375a6bb2d4f5b6e3ae5b435
Paulo Silva <paulobfsilva@gmail.com>
2022-11-07 21:57:04 +0000

    Add 'KJVSearchResultsCacheIntegrationTests' target to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ KJVSearch/KJVSearchResultsCacheIntegrationTests/KJVSearchResultsCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  KJVSearchResultsCacheIntegrationTests.swift
+//  KJVSearchResultsCacheIntegrationTests
+//
+//  Created by Paulo Silva on 07/11/2022.
+//
+
+import XCTest
+
+class KJVSearchResultsCacheIntegrationTests: XCTestCase {
+
+
+}
