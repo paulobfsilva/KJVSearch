ac6392f1c8c06c4a10f49787767efa67e2b4dd35
Paulo Silva <paulobfsilva@gmail.com>
2022-11-11 23:00:40 +0000

    Add 'SearchResultCell' with storyboard connections

---
+++ KJVSearch/SearchResultCell.swift
@@ -0,0 +1,15 @@
+//
+//  SearchResultCell.swift
+//  KJVSearch
+//
+//  Created by Paulo Silva on 11/11/2022.
+//
+
+import UIKit
+
+final class SearchResultCell: UITableViewCell {
+    @IBOutlet private(set) var percentageImage: UIImageView!
+    @IBOutlet private(set) var scriptureVerseLabel: UILabel!
+    @IBOutlet private(set) var scriptureTextLabel: UILabel!
+
+}
